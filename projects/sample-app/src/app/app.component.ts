9e59484cc7725e535805219a1da1096c854762ef
Amadej Bukorović <amadej@e-efekt.net>
2025-01-20 10:46:10 +0100

    Add example of library source mapping issue

--- projects/sample-app/src/app/app.component.ts
+++ projects/sample-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {SampleLibComponent} from 'sample-lib';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SampleLibComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
